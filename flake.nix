f788f94d78bb9411ba294585f21abf5791529b38
SvelteRust <oddharald@myhren.ai>
2025-07-30 11:14:42 +0300

    Add flake

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+    }:
+    let
+      # Setup packages
+      system = "x86_64-linux";
+      overlays = [ rust-overlay.overlays.default ];
+      pkgs = import nixpkgs { inherit system overlays; };
+
+      # Rust toolchain with cross compilation support
+      rust = pkgs.rust-bin.nightly.latest.default.override {
+        extensions = [ "rust-src" ];
+      };
+
+      # System dependencies
+      buildInputs = with pkgs; [
+        pkg-config
+        rust
+        libfprint
+      ];
+    in
+    {
+      devShells.${system} = {
+        default = pkgs.mkShell.override { stdenv = pkgs.llvmPackages.stdenv; } {
+          inherit buildInputs;
+          shellHook = with pkgs; ''
+            export LD_LIBRARY_PATH="${lib.makeLibraryPath buildInputs}"
+            export LIBCLANG_PATH="${llvmPackages.libclang.lib}/lib"
+          '';
+        };
+      };
+    };
+}
